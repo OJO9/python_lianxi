b1b539e20e8a97fd1df97bcfc9758e6fb32756ba
10 <yaOJO9@163.com>
2025-08-04 16:10:28 +0800

    main

--- test.py
+++ test.py
@@ -1,9 +1,14 @@
 #-*- coding: utf-8 -*-
 
-r = 4 > 2 ** 4 or True is 1 and '4' in "345"
-r1 = 4 > 2 ** 4 or True is 1 and '4' in "345"
-print('r1', r)
-if r:
-    print('猜猜我的结果', r)
-else:
-    print('猜猜我的结果2', r)
+# r = 4 > 2 ** 4 or True is 1 and '4' in "345"
+# r1 = 4 > 2 ** 4 or True is 1 and '4' in "345"
+# print('r1', r)
+# if r:
+#     print('猜猜我的结果', r)
+# else:
+#     print('猜猜我的结果2', r)
+
+# a = input("请输入 a：")
+# b = input("请输入 b：")
+# max_number = a if a > b else b
+# print(max_number)
